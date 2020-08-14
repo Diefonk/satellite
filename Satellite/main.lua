d813095b0651dc3a0e48244442688f8602cc417a
Diefonk <thediefonk@gmail.com>
2020-08-14 20:16:14 +0000

    Added start of channels Drawing and toggle mute

--- Satellite/main.lua
+++ Satellite/main.lua
@@ -2,9 +2,13 @@ import "CoreLibs/sprites"
 
 local gfx <const> = playdate.graphics
 
-local center
+local channelSelection
 local channelImages = {}
 local currentChannel
+local channels = {}
+local channelImage
+local channelMuteImage
+local activeChannels = 0
 
 function getCurrentChannel()
 	local crankPosition = playdate.getCrankPosition()
@@ -28,14 +32,32 @@ function getCurrentChannel()
 end
 
 function init()
+	gfx.clear()
 	currentChannel = getCurrentChannel()
 	for index = 1, 8 do
 		channelImages[index] = gfx.image.new("images/channel" .. index)
 	end
-	center = gfx.sprite.new()
-	center:setImage(channelImages[currentChannel])
-	center:moveTo(280, 120)
-	center:add()
+	channelSelection = gfx.sprite.new()
+	channelSelection:setImage(channelImages[currentChannel])
+	channelSelection:moveTo(280, 120)
+	channelSelection:add()
+	channelImage = gfx.image.new("images/channel")
+	channelMuteImage = gfx.image.new("images/channelMute")
+	for index = 1, 8 do
+		channels[index] = {}
+		channels[index].sprite = gfx.sprite.new()
+		channels[index].sprite:setImage(channelMuteImage)
+		channels[index].sprite:add()
+		channels[index].mute = true
+	end
+	channels[1].sprite:moveTo(280, 32)
+	channels[2].sprite:moveTo(342, 58)
+	channels[3].sprite:moveTo(368, 120)
+	channels[4].sprite:moveTo(342, 182)
+	channels[5].sprite:moveTo(280, 208)
+	channels[6].sprite:moveTo(218, 182)
+	channels[7].sprite:moveTo(192, 120)
+	channels[8].sprite:moveTo(218, 58)
 end
 
 function playdate.update()
@@ -46,7 +68,34 @@ function playdate.cranked()
 	local newChannel = getCurrentChannel()
 	if newChannel ~= currentChannel then
 		currentChannel = newChannel
-		center:setImage(channelImages[currentChannel])
+		channelSelection:setImage(channelImages[currentChannel])
+	end
+end
+
+function playdate.AButtonDown()
+	channels[currentChannel].mute = not channels[currentChannel].mute
+	if channels[currentChannel].mute then
+		channels[currentChannel].sprite:setImage(channelMuteImage)
+		activeChannels -= 1
+	else
+		channels[currentChannel].sprite:setImage(channelImage)
+		activeChannels += 1
+	end
+end
+
+function playdate.BButtonDown()
+	if activeChannels > 0 then
+		for index = 1, 8 do
+			channels[index].mute = true
+			channels[index].sprite:setImage(channelMuteImage)
+		end
+		activeChannels = 0
+	else
+		for index = 1, 8 do
+			channels[index].mute = false
+			channels[index].sprite:setImage(channelImage)
+		end
+		activeChannels = 8
 	end
 end
 
