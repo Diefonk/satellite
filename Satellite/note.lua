baacdfe78499856792eccdcfdce9096e955bc2f7
Diefonk <thediefonk@gmail.com>
2020-08-25 17:48:33 +0000

    Added piano images to note selection

--- Satellite/note.lua
+++ Satellite/note.lua
@@ -11,6 +11,8 @@ local notes = {
 	"C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
 	"C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7", "C8"
 }
+local noteImages = {}
+local piano
 
 note = {}
 
@@ -20,14 +22,33 @@ function note.init()
 	label:setImage(labelImage)
 	label:setCenter(0, 1)
 	label:moveTo(5, 235)
+	noteImages[1] = gfx.image.new("images/notes/a")
+	noteImages[2] = gfx.image.new("images/notes/a#")
+	noteImages[3] = gfx.image.new("images/notes/b")
+	noteImages[4] = gfx.image.new("images/notes/c")
+	noteImages[5] = gfx.image.new("images/notes/c#")
+	noteImages[6] = gfx.image.new("images/notes/d")
+	noteImages[7] = gfx.image.new("images/notes/d#")
+	noteImages[8] = gfx.image.new("images/notes/e")
+	noteImages[9] = gfx.image.new("images/notes/f")
+	noteImages[10] = gfx.image.new("images/notes/f#")
+	noteImages[11] = gfx.image.new("images/notes/g")
+	noteImages[12] = gfx.image.new("images/notes/g#")
+	piano = gfx.sprite.new()
+	piano:setImage(noteImages[4])
+	piano:setCenter(0, 1)
+	piano:moveTo(34, 206)
 end
 
 function note.show(data)
 	label:add()
+	note.update(data)
+	piano:add()
 end
 
 function note.hide()
 	label:remove()
+	piano:remove()
 end
 
 function note.getText(data)
@@ -35,6 +56,11 @@ function note.getText(data)
 end
 
 function note.update(data)
+	local index = data.note
+	while index > 12 do
+		index -= 12
+	end
+	piano:setImage(noteImages[index])
 end
 
 function note.up(channel, data)
@@ -44,6 +70,7 @@ function note.up(channel, data)
 	data.note += 1
 	data.pitch = 2 ^ ((data.note - 49) / 12) * 440
 	channel.pitch = data.pitch
+	note.update(data)
 end
 
 function note.down(channel, data)
@@ -53,4 +80,5 @@ function note.down(channel, data)
 	data.note -= 1
 	data.pitch = 2 ^ ((data.note - 49) / 12) * 440
 	channel.pitch = data.pitch
+	note.update(data)
 end
